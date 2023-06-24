af967ed8bc88fc0e51fdb8689b730b5e78275c26
AlonsoDsc <137337998+AlonsoDsc@users.noreply.github.com>
2023-06-24 01:45:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,34 @@ const content = document.querySelector(".content");
 const contentFinish = document.querySelector(".finish");
 const btnRestart = document.querySelector(".restart");
 const btnCopy = document.querySelector(".copy");
+const spnScore = document.querySelector(".scoreRealTime");
+const form = document.getElementById("form")
 const name = document.getElementById("name");
 const rg = document.getElementById("rg");
 
 import questions from "./questions.js";
+
+function shuffle(array) {
+  var m = array.length, t, i;
+
+  while (m) {
+
+    i = Math.floor(Math.random() * m--);
+
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+shuffle(questions)
 
 let currentIndex = 0;
 let questionsCorrect = 0;
 
 function nextQuestion(e) {
   if (e.target.getAttribute("data-correct") === "true") {
-    var randomQuestion = questions[Math.floor(Math.random()*questions.length)]
-    // console.log(randomQuestion)
     questionsCorrect++;
   }
 
@@ -32,10 +48,11 @@ function nextQuestion(e) {
 function finish() {
   if(questionsCorrect >= Math.round(questions.length * 0.7)) {
     textFinish.innerHTML = `Aprovado, acertou ${questionsCorrect} de ${questions.length}`;
-    textFinish.style.color = "green"
+    textFinish.style.color = "#50C878"
     content.style.display = "none";
     contentFinish.style.display = "flex";
     btnCopy.onclick = () => {
+      alert("Copiado com sucesso!")
       navigator.clipboard.writeText(`\`\`\`\nNOME CONSCRITO: ${name.value}\nRG CONSCRITO: ${rg.value}\nNOTA CONSCRITO: ${questionsCorrect}/${questions.length}\nSTATUS: Aprovado\`\`\``)
       btnCopy.style.color = "green"
       btnCopy.innerHTML = "Copiado ✅"
@@ -46,8 +63,9 @@ function finish() {
     content.style.display = "none";
     contentFinish.style.display = "flex";
     btnCopy.onclick = () => {
-      navigator.clipboard.writeText(`\`\`\`\nNOME CONSCRITO: ${name.value}\nRG CONSCRITO: ${rg.value}\nNOTA CONSCRITO: ${questionsCorrect}/${questions.length}\nSTATUS: Reprovado\`\`\``)
-      btnCopy.style.color = "green"
+      alert("Copiado com sucesso!")
+      navigator.clipboard.writeText(`\`\`\`\nNOME CONSCRITO: ${name.value || "nulo"}\nRG CONSCRITO: ${rg.value || "nulo"}\nNOTA CONSCRITO: ${questionsCorrect}/${questions.length}\nSTATUS: Reprovado\`\`\``)
+      btnCopy.style.color = "#50C878"
       btnCopy.innerHTML = "Copiado ✅"
     }
   }
@@ -55,19 +73,20 @@ function finish() {
 
 function loadQuestion() {
   spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  spnScore.innerHTML = `Acertou ${questionsCorrect} de ${questions.length}`
+  spnScore.style.fontSize = "20px"
+  spnScore.style.color = "#00FF00"
   const item = questions[currentIndex];
   answers.innerHTML = "";
   question.innerHTML = item.question;
 
   item.answers.forEach((answer) => {
     const div = document.createElement("div");
-
     div.innerHTML = `
     <button class="answer" data-correct="${answer.correct}">
       ${answer.option}
     </button>
     `;
-
     answers.appendChild(div);
   });
 
@@ -79,13 +98,11 @@ function loadQuestion() {
 }
 
 btnRestart.onclick = () => {
-  content.style.display = "flex";
-  contentFinish.style.display = "none";
-
-  currentIndex = 0;
-  questionsCorrect = 0;
-  // loadQuestion();
-  location.reload()
+  if(confirm("Tem certeza que deseja reiniciar?") === true) {
+    location.reload()
+  } else{
+    return
+  }
 };
 
 loadQuestion();
